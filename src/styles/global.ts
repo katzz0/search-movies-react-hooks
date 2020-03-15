f2665c2d803515aded463440157c115237ee8573
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-03-15 12:58:03 +0100

    Make body and root element 100% height

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,17 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
   body {
     width: 100%;
     height: 100%;
-    overflow-x: hidden;
-    overflow-y: scroll;
+    overflow: hidden;
+    
+    > * {
+      height: 100%;
+    }
   }
 `
