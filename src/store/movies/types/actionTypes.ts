aceb6f2fbc24f3071abb108198750332a1d0bd18
Maxim Palenov <palenov.maxim@gmail.com>
2021-02-11 10:32:56 -0300

    enable no-shadow eslint rule

--- src/store/movies/types/actionTypes.ts
+++ src/store/movies/types/actionTypes.ts
@@ -1,3 +1,5 @@
+// fix a false positive error by eslint
+// eslint-disable-next-line no-shadow
 export enum MoviesActionTypes {
   SEARCH_REQUEST = '@@movies/SEARCH_REQUEST',
   SEARCH_SUCCESS = '@@movies/SEARCH_SUCCESS',
